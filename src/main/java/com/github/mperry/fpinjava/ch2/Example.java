e64536fcbc69a94d7cd14e2ca5439278cd977157
Mark Perry <maperry78+github@gmail.com>
2015-07-23 16:12:08 +1000

    Created exercises and solution after presenting to off shore team

--- src/main/java/com/github/mperry/fpinjava/ch2/Example.java
+++ src/main/java/com/github/mperry/fpinjava/ch2/Example.java
@@ -1,16 +1,25 @@
 package com.github.mperry.fpinjava.ch2;
 
-import fj.F;
+import fj.F2;
 
-import static java.lang.System.out;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by MarkPerry on 13/07/2015.
  */
 public class Example {
 
-    static <A, B, C> F<A, C> andThen(F<A, B> f, F<B, C> g) {
-        return a -> g.f(f.f(a));
+    static void examples() {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        int sum = fold(list, 0, (a, acc) -> a + acc);
+        int product = fold(list, 1, (a, acc) -> a * acc);
+        String s = fold(list, "", (a, acc) -> acc + a);
+    }
+
+    static <A, B> B fold(List<A> list, B b, F2<A, B, B> f) {
+        // TODO: implementation
+        return null;
     }
 
 }
