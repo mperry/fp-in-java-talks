bdc0244fc03cc84a954d06f5cb79a055f7c47687
Mark Perry <maperry78@yahoo.com.au>
2015-08-05 00:22:39 +1000

    Slides and exercises for validations

--- src/main/java/com/github/mperry/fpinjava/ch4/Exercises.java
+++ src/main/java/com/github/mperry/fpinjava/ch4/Exercises.java
@@ -4,24 +4,73 @@ import fj.F;
 import fj.F2;
 import fj.data.List;
 import fj.data.Option;
+import fj.data.Validation;
 
 /**
  * Created by MarkPerry on 4/08/2015.
  */
 public class Exercises {
 
+    // Option methods
+
+    static <A, B> Option<B> map(Option<A> oa, F<A, B> f) {
+        // TODO: implement
+        return null;
+    }
+
+    static <A> Option<A> filter(Option<A> oa, F<A, Boolean> f) {
+        // TODO: implement
+        return null;
+    }
+
+    static <A, B> Option<B> bind(Option<A> oa, F<A, Option<B>> f) {
+        // TODO: implement
+        return null;
+    }
 
     static <A, B, C> Option<C> liftM2(Option<A> oa, Option<B> ob, F2<A, B, C> f) {
         // TODO: implement
         return null;
     }
 
-    static <A> Option<List<A>> sequence(List<Option<A>> list) {
+    static <A> Option<List<A>> sequenceOption(List<Option<A>> list) {
         // TODO: implement
         return null;
     }
 
-    static <A, B> Option<List<B>> traverse(List<A> list, F<A, Option<B>> f) {
+    static <A, B> Option<List<B>> traverseOption(List<A> list, F<A, Option<B>> f) {
+        // TODO: implement
+        return null;
+    }
+
+    // Validation methods
+
+    static <E, A, B> Validation<E, B> map(Validation<E, A> v, F<A, B> f) {
+        // TODO: implement
+        return null;
+    }
+
+    static <E, A, B> Validation<E, B> bind(Validation<E, A> v, F<A, Validation<E, B>> f) {
+        // TODO: implement
+        return null;
+    }
+
+    static <E, A> Validation<E, A> orElse(Validation<E, A> v1, Validation<E, A> v2) {
+        // TODO: implement
+        return null;
+    }
+
+    static <E, A, B, C> Validation<E, C> liftM2(Validation<E, A> v1, Validation<E, B> v2, F2<A, B, C> f) {
+        // TODO: implement
+        return null;
+    }
+
+    static <E, A> Validation<E, List<A>> sequenceValidation(List<Validation<E, A>> list) {
+        // TODO: implement
+        return null;
+    }
+
+    static <E, A, B> Validation<E, List<B>> traverseValidation(List<A> list, F<A, Validation<E, B>> f) {
         // TODO: implement
         return null;
     }
--- src/main/java/com/github/mperry/fpinjava/ch4/Solutions.java
+++ src/main/java/com/github/mperry/fpinjava/ch4/Solutions.java
@@ -4,6 +4,7 @@ import fj.F;
 import fj.F2;
 import fj.data.List;
 import fj.data.Option;
+import fj.data.Validation;
 
 /**
  * Created by MarkPerry on 4/08/2015.
@@ -11,18 +12,62 @@ import fj.data.Option;
 public class Solutions {
 
 
+    // Option methods
+
+    static <A, B> Option<B> map(Option<A> oa, F<A, B> f) {
+        return oa.isNone() ? Option.<B>none() : Option.some(f.f(oa.some()));
+    }
+
+    static <A> Option<A> filter(Option<A> oa, F<A, Boolean> f) {
+        return oa.isSome() && f.f(oa.some()) ? oa : Option.<A>none();
+    }
+
+    static <A, B> Option<B> bind(Option<A> oa, F<A, Option<B>> f) {
+        return oa.isNone() ? Option.<B>none() : f.f(oa.some());
+    }
+
     static <A, B, C> Option<C> liftM2(Option<A> oa, Option<B> ob, F2<A, B, C> f) {
         return oa.bind(a -> ob.map(b -> f.f(a, b)));
     }
 
-    static <A> Option<List<A>> sequence(List<Option<A>> list) {
+    static <A> Option<List<A>> sequenceOption(List<Option<A>> list) {
         return list.foldRight((oa, acc) -> acc.bind(la -> oa.map(a -> la.cons(a))),
                 Option.some(List.<A>nil())
         );
     }
 
-    static <A, B> Option<List<B>> traverse(List<A> list, F<A, Option<B>> f) {
-        return sequence(list.map(f));
+    static <A, B> Option<List<B>> traverseOption(List<A> list, F<A, Option<B>> f) {
+        return sequenceOption(list.map(f));
+    }
+
+    // Validation methods
+
+    static <E, A, B> Validation<E, B> map(Validation<E, A> v, F<A, B> f) {
+        return v.isFail() ?
+                Validation.fail(v.fail()) :
+                Validation.success(f.f(v.success()));
+    }
+
+    static <E, A, B> Validation<E, B> bind(Validation<E, A> v, F<A, Validation<E, B>> f) {
+        return v.isFail() ? Validation.fail(v.fail()) : f.f(v.success());
+    }
+
+    static <E, A> Validation<E, A> orElse(Validation<E, A> v1, Validation<E, A> v2) {
+        return v1.isFail() ? v1 : v2;
+    }
+
+    static <E, A, B, C> Validation<E, C> liftM2(Validation<E, A> v1, Validation<E, B> v2, F2<A, B, C> f) {
+        return bind(v1, a -> v2.map(b -> f.f(a, b)));
+    }
+
+    static <E, A> Validation<E, List<A>> sequenceValidation(List<Validation<E, A>> list) {
+        return list.foldLeft((Validation<E, List<A>> acc, Validation<E, A> v) ->
+                        acc.bind(la -> v.map(a -> la.cons(a))), Validation.success(List.nil())
+        );
+    }
+
+    static <E, A, B> Validation<E, List<B>> traverseValidation(List<A> list, F<A, Validation<E, B>> f) {
+        return sequenceValidation(list.map(f));
     }
 
 }
