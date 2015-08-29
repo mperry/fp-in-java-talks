8d3118f4a9c4466a7fecb2b3e5b62d6d7e8cc77d
Mark Perry <maperry78@yahoo.com.au>
2015-08-29 10:13:02 +1000

    Ch 7 exercises and solutions setup

---
+++ src/main/java/com/github/mperry/fpinjava/ch7/Exercises.java
@@ -0,0 +1,10 @@
+package com.github.mperry.fpinjava.ch7;
+
+/**
+ * Created by MarkPerry on 29/08/2015.
+ */
+public class Exercises {
+
+
+
+}
---
+++ src/main/java/com/github/mperry/fpinjava/ch7/Solutions.java
@@ -0,0 +1,9 @@
+package com.github.mperry.fpinjava.ch7;
+
+/**
+ * Created by MarkPerry on 29/08/2015.
+ */
+public class Solutions {
+
+
+}
