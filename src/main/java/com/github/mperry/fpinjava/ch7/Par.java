c7473ba6ca9fbe0458fb68380a4540fbe395d7fc
Mark Perry <maperry78@yahoo.com.au>
2015-10-11 21:53:53 +1000

    Finished section 7.1.1

---
+++ src/main/java/com/github/mperry/fpinjava/ch7/Par.java
@@ -0,0 +1,19 @@
+package com.github.mperry.fpinjava.ch7;
+
+import fj.F0;
+import fj.P1;
+
+/**
+ * Created by MarkPerry on 10/10/2015.
+ */
+public class Par<A> {
+
+    public static <A> Par<A> unit(F0<A> p) {
+        return null;
+    }
+
+    public A get() {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/github/mperry/fpinjava/ch7/DemoTest.java
@@ -0,0 +1,37 @@
+package com.github.mperry.fpinjava.ch7;
+
+import fj.P2;
+import fj.data.Seq;
+
+/**
+ * Created by MarkPerry on 10/10/2015.
+ */
+public class DemoTest {
+
+    static int sum(Seq<Integer> ints) {
+        return ints.foldLeft((a, b) -> a + b, 0);
+    }
+
+    static int sum2(Seq<Integer> ints) {
+        int n = ints.length();
+        if (n <= 1) {
+            return ints.headOption().orSome(0);
+        } else {
+            P2<Seq<Integer>, Seq<Integer>> p = ints.split(n / 2);
+            return sum2(p._1()) + sum2(p._2());
+        }
+    }
+
+    static int sum3(Seq<Integer> ints) {
+        int n = ints.length();
+        if (n <= 1) {
+            return ints.headOption().orSome(0);
+        } else {
+            P2<Seq<Integer>, Seq<Integer>> p = ints.split(n / 2);
+            Par<Integer> l = Par.unit(() -> sum3(p._1()));
+            Par<Integer> r = Par.unit(() -> sum3(p._2()));
+            return l.get() + r.get();
+        }
+    }
+
+}
